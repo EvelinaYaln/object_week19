c5a355e614865b0f8477e8eafa5ddda493c14639
EvelinaYaln <evelina.dranets@gmail.com>
2022-02-23 17:02:43 +0300

    add changes in names

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-//function bike() {
+function bike() {
+    console.log(this.name);
+}
 
-//}
+let name = 'ninja';
+let obj1 = {
+    name = 'pomidor',
+    bike = 'bike'
+}
+let obj2 = {
+    name = 'site',
+    bike = 'bike'
+}
+
+bike();
+obj1.bike();
+obj2.bike();
